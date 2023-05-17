7a7f51a7b2221b757a3f3b301f4a397d24644f5b
Kongsis <lee_scyj@nate.com>
2023-05-17 16:59:34 +0900

    setting

---
+++ src/main/java/com/icia/bookstore/repository/BookstoreRepository.java
@@ -0,0 +1,7 @@
+package com.icia.bookstore.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookstoreRepository {
+}
---
+++ src/main/java/com/icia/bookstore/service/BookstoreService.java
@@ -0,0 +1,7 @@
+package com.icia.bookstore.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookstoreService {
+}
